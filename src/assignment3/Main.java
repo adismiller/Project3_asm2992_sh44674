56a6ab2a7a0932e58ac6c39972731f8fa8198069
Pranav Harathi <phrocks254@gmail.com>
2016-09-28 14:49:15 -0500

    Modified DFS to work with some test cases, functionality still incomplete

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -55,16 +55,39 @@ public class Main {
 		return null;
 	}
 	
-	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		
-		// Returned list should be ordered start to end.  Include start and end.
-		// Return empty list if no ladder.
-		// TODO some code
-		Set<String> dict = makeDictionary();
-		// TODO more code
-		
-		return null; // replace this line later with real return
-	}
+    public static ArrayList<String> getWordLadderDFS(String start, String end) {
+        
+        // Returned list should be ordered start to end.  Include start and end.
+        // Return empty list if no ladder.
+        // TODO some code
+        Set<String> dict = makeDictionary();
+        // TODO more code
+        Set<String> visited = new HashSet<>();
+        ArrayList<String> path = new ArrayList<>();
+        path.add(start);
+        DFShelper(start, end, visited, path);
+        //System.out.println(path);
+        return path; // replace this line later with real return
+    }
+    
+    public static void DFShelper(String input, String end, Set<String> visited, ArrayList<String> path) {
+        //System.out.println(input);
+        if(input.equalsIgnoreCase(end)) {
+            System.out.println("success!");
+            return;
+        }
+        visited.add(input);
+        int[] differences = getDifferentIndices(input, end);
+        String nextInput = getNextMutantWord(input, visited, differences);
+        if(nextInput.equals("")) {
+            //System.out.println(path);
+            //path.remove(path.size()-1);
+            DFShelper(path.remove(path.size()-1), end, visited, path);
+        } else {
+            path.add(nextInput);
+            DFShelper(nextInput, end, visited, path);
+        }
+    }
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
 		
@@ -113,26 +136,51 @@ public class Main {
      * @param visitedWords
      * @return
      */
-    public static String getNextMutantWord(String source, Set<String> visitedWords) {
-        for(int i = 0; i < source.length(); i++) {
-            for(char c = 'a'; c <= 'z'; c++) {
-                String ch = Character.toString(c);
+    public static String getNextMutantWord(String source, Set<String> visitedWords, int[] diffs) {
+        List<Integer> randomizedIndices = randomizedNums(source.length(), 0);
+        for(int i : randomizedIndices) {
+            if(diffs[i] == 0) continue;
+            List<Integer> randomizedChars = randomizedNums(26, (int)'a');
+            for(Integer c : randomizedChars) {
+                String ch = Character.toString((char) c.intValue());
                 String word = source.substring(0, i) + ch + source.substring(i+1);
-                if(!visitedWords.contains(word) && dictionary.contains(word)) {
+                if(!visitedWords.contains(word) && dictionary.contains(word.toUpperCase())) {
                     return word;
                 }
             }
+            System.out.println();
         }
         return "";
     }
+
+	/**
+	 * DFS heuristic that limits search for mutant words to only include the indices of
+	 * the string that are different
+	 * @param input
+	 * @param end
+	 * @return
+	 */
+	public static int[] getDifferentIndices(String input, String end) {
+		int[] diffs = new int[end.length()];
+		for(int i = 0; i < diffs.length; i++) {
+			if(input.charAt(i) != end.charAt(i)) diffs[i] = 1;
+		}
+		return diffs;
+	}
 	
-	/*public ... dfs(String source, String destination)
-	{
-		ArrayList<String> mutants= getAllMutantsOfWord(source);
-		for(int i=0; i<mutants.size(); i++)
-			if mutant.get(i);
-		
-	}*/
+	/**
+	 * DFS heuristic to introduce randomization and slightly improve performance 
+	 * for worst case
+	 * @return
+	 */
+	public static List<Integer> randomizedNums(int size, int add) {
+		List<Integer> randomizedNums = new ArrayList<>();
+		for(int i1 = 0; i1 < size; i1++) {
+			randomizedNums.add(new Integer(i1 + add));
+		}
+		Collections.shuffle(randomizedNums);
+		return randomizedNums;
+	}
 	
 	// TODO
 	// Other private static methods here
